9e9c243926d0334e18d3f05600e6b9445305181b
David Šenica <david.senica@medius.si>
2021-05-22 22:44:28 +0200

    Fixed login/registration

--- src/main/java/si/red/dragons/api/LoginResource.java
+++ src/main/java/si/red/dragons/api/LoginResource.java
@@ -8,6 +8,7 @@ import si.red.dragons.utils.JWTUtils;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -17,17 +18,15 @@ public class LoginResource {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
     public Response login(LoginDTO login) throws Exception {
         Account account = Account.find("email", login.getEmail()).firstResult();
-
         if (account == null){
             // User not found
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
-
         String hashedPass = CryptoUtils.sha512(login.getPassword());
-
-        if(hashedPass != account.getPassword()){
+        if (!hashedPass.equals(account.getPassword())) {
             // Incorrect password
             return Response.status(Response.Status.UNAUTHORIZED).build();
         }
