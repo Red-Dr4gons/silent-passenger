710bd06cc83f1550bcb353e113df5a6502501de5
David Šenica <david.senica@medius.si>
2021-05-22 16:49:17 +0200

    Login dialog

---
+++ src/main/frontend/src/app/authentication/login-signup.dialog.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'login-signup-dialog',
+  templateUrl: 'login-signup.dialog.html',
+  styleUrls: []
+})
+export class LoginSignupDialog {
+}
